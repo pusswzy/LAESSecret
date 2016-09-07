52e28f193946df5aed0b7d4f2c8fe9434342c6b5
pusswzy <Li@ze.lan>
2016-09-07 17:08:54 +0800

    firsr commit

---
+++ LAESSecret/LAESSecret/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  LAESSecret
+//
+//  Created by 李昊泽 on 16/9/7.
+//  Copyright © 2016年 李昊泽. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
